942279a0599108c85c5b54bd7ac284c7172c1494
Matheus Andrade <matheus61@hotmail.com>
2020-05-20 16:06:28 -0300

    Updates

---
+++ precache-manifest.1068b14250e59be08d4ca873cbc86b18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31e283bff95dab7e5a1a8b9b3e31e11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "566dc8d569159c69e206",
+    "url": "/static/js/2.453af4b9.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.453af4b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd373a6736bf45eeefdc",
+    "url": "/static/js/main.8b35b374.chunk.js"
+  },
+  {
+    "revision": "60981e5543f4667304d1",
+    "url": "/static/js/runtime-main.87efdd8b.js"
+  }
+]);
